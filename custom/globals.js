8dd49db4ab14903e7ab0c4925d4d30626ae16730
wharding <wharding@qualityworkscg.com>
2016-12-28 13:44:43 -0500

    Added globals file

---
+++ custom/globals.js
@@ -0,0 +1,14 @@
+/**
+  * Define your project-wide globals here 
+  * 
+  */
+
+export default {
+
+  // Sets the default timeout for all actions to 10 secounds
+  waitForConditionTimeout: 10000,
+
+
+  // Prevents nightwatch from aborting a test suite if a single test fails
+  abortOnAssertionFailure: false
+}
