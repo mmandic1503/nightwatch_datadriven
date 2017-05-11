73f4f7009af6ecf1410fc352daa5cc2824f86bac
wharding <wharding@qualityworkscg.com>
2017-05-11 14:26:02 -0500

    updated nightwatch to use mocha

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,34 +1,19 @@
-/**
- * Each test file may should be considered as a test suite and may contain one or * test cases. The 'beforeEach' hook may be used to run sections of code/logic before the indifivual tests
- * 
- */
 
+describe('Mocha with nightwatch', function () {
+  const x = ['Test 1', 'Test 2']
+  x.forEach(testName => {
+    it(`${testName}`, function (browser) {
+      const navbar = browser.page.navbar();
+      navbar
+        .navigate()
+        .clickAgileCoachingLink()
+        .assert.title('QualityWorks Consulting Group | Agile Coaching and Training');
+    });
+  });
+});
 
-//Export the entire file to ensure it is found my nightwatch
-export default {
-
-  /**
-   * Nightwatch functions generally take a browser parameter to interact with th ebrowser
-  */
-  before(browser) {
-    browser.maximizeWindow();
-  },
-
-  'Testing the Title of QualityWorks Homepage': browser => {
-
-    // Initialize the constant navbar to the page-object navbar
-    const navbar = browser.page.navbar();
-
-    // See navbar.js for implementation details of clickAgileCoackingLink method
-    navbar
-      .navigate()
-      .clickAgileCoachingLink()
-      //Chained Assertion
-      .assert.title('QualityWorks Consulting Group | Agile Coaching and Training');
-  },
-
-  // Close the browser after test completion, failure or timeout
-  after(browser) {
-    browser.end();
-  }
-}
+after(function (client, done) {
+  client.end(function () {
+    done();
+  });
+});
